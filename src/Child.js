c8fe178d116018be5c0e10a4cbc39ed8125074ee
Mahdy Noureddine <mahdy.a.noureddine@gmail.com>
2021-12-16 19:40:09 -0500

    commit tree 6ca38074bed24b0bc43d1e2b499b794780780c7c

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, color }) {  // (1) We add the passed down callback function as the click event function, this will update the state of the color in the parent company and re-render the parent with a new color
+                                    // (2) we are setting the background color of the child to whatever current child color state the parent is passing down
+  
+  function handleClick() {   // (2) we removed the onChangeColor from the click event in the child and instead added it inside of another callback function that will be invoked when clicked, this way, a random child color will be generated here and passed as an argument into the parent component, the function in there will then randomly generate a color for the parent, changing the state of both but to different colors
+    const newChildColor = getRandomColor()
+    onChangeColor(newChildColor)
+  }
+  
+return( 
+  <div 
+  className="child" 
+  style={{ backgroundColor: color }}
+  onClick = {handleClick}
+  />);
 }
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -5,11 +5,23 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childColor, setChildColor] = useState("#FFF")
+// (2) We now want to make it so that whenever either of the children components are clicked, we want the clicked child's color to randomly change as well as the other child to that same color, we can't pass states in between children so the color state for the children must be set here, with a callback function that will change this state once a child is clicked
 
-  return (
+// (1)We want to make it so that whenever a child component is clicked, the state of this parent component will change, this is done by creating a function here and passing this callback function as a prop and adding it as the callback function that a click event will trigger
+
+                                // (2) this callback function must now be altered to include the newChild color that will be randomized in the child component and used as an argument when invoking this function as a callback, both the parent and child colors will be randomized separately this way whenever either child is clicked and their inner states updated
+function handleChangeColor(newChildColor) {   // (1) every time one of the child components are clicked, it will trigger this callback function, producing a new random color and using the setter function to change the inner state of color, re-rendering this parent component
+  const newRandomColor = getRandomColor()
+  setColor(newRandomColor)
+  setChildColor(newChildColor)
+}
+
+              // (2) the childColor is passed down to the children as a prop from the parent, initially it will be white (#FFF)
+  return (     // (1) callback function is passed down to the children components as props to give them access to this parents state
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child onChangeColor={handleChangeColor} color= {childColor}/>
+      <Child onChangeColor={handleChangeColor} color= {childColor}/>
     </div>
   );
 }
